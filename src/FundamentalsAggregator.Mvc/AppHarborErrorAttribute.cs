d1d1725a82a4708f009d077343285be3a07e24f4
Richard Dingwall <rdingwall@gmail.com>
2013-04-17 16:25:21 +0100

    Added comments

--- src/FundamentalsAggregator.Mvc/AppHarborErrorAttribute.cs
+++ src/FundamentalsAggregator.Mvc/AppHarborErrorAttribute.cs
@@ -6,6 +6,7 @@ namespace FundamentalsAggregator.Mvc
     {
         public void OnException(ExceptionContext filterContext)
         {
+            // From http://blog.appharbor.com/2011/12/20/super-simple-logging-on-appharbor
             new LogEvent(filterContext.Exception).Raise();
         }
     }
